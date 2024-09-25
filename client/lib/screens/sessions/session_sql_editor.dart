50afcd0eeb3d24cc5f74cf2e7de4a286a02a2c06
Sun Jian <921465802@qq.com>
2024-09-25 13:01:54 +0800

    tmp: 会话按钮改造

--- client/lib/screens/sessions/session_sql_editor.dart
+++ client/lib/screens/sessions/session_sql_editor.dart
@@ -1,5 +1,4 @@
 import 'package:client/providers/sessions.dart';
-import 'package:client/widgets/hover_icon_button.dart';
 import 'package:flutter/material.dart';
 import 'package:code_text_field/code_text_field.dart';
 import 'package:flutter_highlight/themes/a11y-light.dart';
@@ -70,56 +69,15 @@ class _CodeEditorState extends State<CodeEditor> {
         ),
       ));
 
-      Widget codeButton = Container(
-        color: Theme.of(context).colorScheme.surfaceContainerLowest,
-        constraints: const BoxConstraints(maxHeight: 40),
-        child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-          Consumer<SessionProvider>(builder: (context, sessionProvider, _) {
-            final canQuery = sessionProvider.canQuery();
-            return IconButton(
-                alignment: Alignment.topLeft,
-                onPressed: canQuery
-                    ? () {
-                        var content = widget.codeController.text.toString();
-                        List<String> querys = Splitter(content, ";").split();
-                        TextSelection s = widget.codeController.selection;
+      Widget codeButtonBar =
+          CodeButtionBar(codeController: widget.codeController);
 
-                        String query;
-                        // 当界面手动选中了文本片段则仅执行该片段，当前还不支持多SQL执行.
-                        if (!s.isCollapsed) {
-                          query = widget.codeController.text
-                              .toString()
-                              .substring(s.start, s.end);
-                        } else {
-                          // 当前界面未选中SQL, 则当前游标在哪个SQL片段内就执行哪个SQL. todo: 移动到通用的地方.
-                          int totalLength = 0;
-                          query = querys.firstWhere((query) {
-                            totalLength = totalLength + query.length;
-                            if (s.start <= totalLength) {
-                              return true;
-                            } else {
-                              return false;
-                            }
-                          }, orElse: () => "");
-                        }
-                        if (query.trim().isNotEmpty) {
-                          sessionProvider.query(query);
-                        }
-                      }
-                    : null,
-                icon: Icon(Icons.play_arrow_rounded,
-                    size: 36, color: canQuery ? Colors.green : Colors.grey));
-          }),
-          HoverIconButton(onTap: () => print(1), icon: Icons.play_arrow_rounded)
-        ]),
-      );
-
-      if (c.maxHeight <= 40) {
-        codeButton = Expanded(child: codeButton);
-        return Column(children: [codeButton]);
+      if (c.maxHeight <= 36) {
+        codeButtonBar = Expanded(child: codeButtonBar);
+        return Column(children: [codeButtonBar]);
       } else {
         return Column(
-          children: [codeField, codeButton],
+          children: [codeField, codeButtonBar],
         );
       }
     });
@@ -197,6 +155,71 @@ class _SqlResultTablesState extends State<SqlResultTables> {
         ),
         const Expanded(child: SqlResultTable())
       ],
+    );
+  }
+}
+
+class CodeButtionBar extends StatefulWidget {
+  final CodeController codeController;
+  final double? height;
+
+  const CodeButtionBar({Key? key, required this.codeController, this.height})
+      : super(key: key);
+
+  @override
+  State<CodeButtionBar> createState() => _CodeButtionBarState();
+}
+
+class _CodeButtionBarState extends State<CodeButtionBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 5),
+      color: Theme.of(context).colorScheme.surfaceContainerLowest,
+      constraints: BoxConstraints(maxHeight: 36),
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Consumer<SessionProvider>(builder: (context, sessionProvider, _) {
+              final canQuery = sessionProvider.canQuery();
+              return IconButton(
+                  iconSize: 36,
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(2),
+                  onPressed: canQuery
+                      ? () {
+                          var content = widget.codeController.text.toString();
+                          List<String> querys = Splitter(content, ";").split();
+                          TextSelection s = widget.codeController.selection;
+
+                          String query;
+                          // 当界面手动选中了文本片段则仅执行该片段，当前还不支持多SQL执行.
+                          if (!s.isCollapsed) {
+                            query = widget.codeController.text
+                                .toString()
+                                .substring(s.start, s.end);
+                          } else {
+                            // 当前界面未选中SQL, 则当前游标在哪个SQL片段内就执行哪个SQL. todo: 移动到通用的地方.
+                            int totalLength = 0;
+                            query = querys.firstWhere((query) {
+                              totalLength = totalLength + query.length;
+                              if (s.start <= totalLength) {
+                                return true;
+                              } else {
+                                return false;
+                              }
+                            }, orElse: () => "");
+                          }
+                          if (query.trim().isNotEmpty) {
+                            sessionProvider.query(query);
+                          }
+                        }
+                      : null,
+                  icon: Icon(Icons.play_arrow_rounded,
+                      color: canQuery ? Colors.green : Colors.grey));
+            }),
+          ]),
     );
   }
 }
