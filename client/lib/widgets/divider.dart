9599e848066d2d2f956d658bdaf5a128d8461049
Sun Jian <921465802@qq.com>
2025-10-21 12:59:57 +0800

    divider 组件代码简化

--- client/lib/widgets/divider.dart
+++ client/lib/widgets/divider.dart
@@ -1,28 +1,22 @@
 import 'package:flutter/material.dart';
 
-
 /// 像素级分割线，取消抗锯齿效果
 /// 与Flutter标准库Divider定义一致，但提供更清晰的线条渲染
 class PixelDivider extends StatelessWidget {
-  /// 分割线的高度
-  final double? height;
-  
   /// 分割线的厚度
   final double? thickness;
-  
+
   /// 分割线的缩进
   final double? indent;
-  
+
   /// 分割线的结束缩进
   final double? endIndent;
-  
+
   /// 分割线的颜色
   final Color? color;
-  
 
   const PixelDivider({
     super.key,
-    this.height,
     this.thickness,
     this.indent,
     this.endIndent,
@@ -31,33 +25,17 @@ class PixelDivider extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final dividerTheme = theme.dividerTheme;
-    
-    final height = this.height ?? dividerTheme.space ?? 1.0;
-    final thickness = this.thickness ?? dividerTheme.thickness ?? 1.0;
-    final indent = this.indent ?? dividerTheme.indent ?? 0.0;
-    final endIndent = this.endIndent ?? dividerTheme.endIndent ?? 0.0;
-    final color = this.color ?? theme.dividerColor;
+    final thickness = this.thickness ?? 1.0;
+    final indent = this.indent ?? 0.0;
+    final endIndent = this.endIndent ?? 0.0;
+    final color = this.color ?? Theme.of(context).dividerColor;
 
-    return SizedBox(
-      height: height,
-      child: Center(
-        child: Container(
-          height: thickness,
-          margin: EdgeInsetsDirectional.only(
-            start: indent,
-            end: endIndent,
-          ),
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              return CustomPaint(
-                painter: PixelLinePainter(color: color, thickness: thickness),
-                size: Size(constraints.maxWidth, thickness),
-              );
-            },
-          ),
-        ),
+    return Container(
+      height: thickness,
+      margin: EdgeInsetsDirectional.only(start: indent, end: endIndent),
+      child: CustomPaint(
+        painter: PixelLinePainter(color: color, thickness: thickness),
+        size: Size(double.infinity, thickness),
       ),
     );
   }
@@ -65,25 +43,20 @@ class PixelDivider extends StatelessWidget {
 
 /// 像素级垂直分割线
 class PixelVerticalDivider extends StatelessWidget {
-  /// 分割线的宽度
-  final double? width;
-  
   /// 分割线的厚度
   final double? thickness;
-  
+
   /// 分割线的缩进
   final double? indent;
-  
+
   /// 分割线的结束缩进
   final double? endIndent;
-  
+
   /// 分割线的颜色
   final Color? color;
-  
 
   const PixelVerticalDivider({
     super.key,
-    this.width,
     this.thickness,
     this.indent,
     this.endIndent,
@@ -92,37 +65,18 @@ class PixelVerticalDivider extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final dividerTheme = theme.dividerTheme;
-    
-    final width = this.width ?? dividerTheme.space ?? 1.0;
-    final thickness = this.thickness ?? dividerTheme.thickness ?? 1.0;
-    final indent = this.indent ?? dividerTheme.indent ?? 0.0;
-    final endIndent = this.endIndent ?? dividerTheme.endIndent ?? 0.0;
-    final color = this.color ?? theme.dividerColor;
+    final thickness = this.thickness ?? 1.0;
+    final indent = this.indent ?? 0.0;
+    final endIndent = this.endIndent ?? 0.0;
+    final color = this.color ?? Theme.of(context).dividerColor;
 
-    return SizedBox(
-      width: width,
-      child: Center(
-        child: Container(
-          width: thickness,
-          margin: EdgeInsets.only(
-            top: indent,
-            bottom: endIndent,
-          ),
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              return CustomPaint(
-                painter: PixelLinePainter(
-                  color: color, 
-                  thickness: thickness,
-                  orientation: Axis.vertical,
-                ),
-                size: Size(thickness, constraints.maxHeight),
-              );
-            },
-          ),
-        ),
+    return Container(
+      width: thickness,
+      margin: EdgeInsets.only(top: indent, bottom: endIndent),
+      child: CustomPaint(
+        painter: PixelLinePainter(
+            color: color, thickness: thickness, orientation: Axis.vertical),
+        size: Size(thickness, double.infinity),
       ),
     );
   }
@@ -136,7 +90,7 @@ class PixelLinePainter extends CustomPainter {
   final Axis orientation;
 
   PixelLinePainter({
-    required this.color, 
+    required this.color,
     this.thickness = 1.0,
     this.orientation = Axis.horizontal,
   });
@@ -170,8 +124,8 @@ class PixelLinePainter extends CustomPainter {
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return oldDelegate is PixelLinePainter && 
-           (oldDelegate.color != color || 
+    return oldDelegate is PixelLinePainter &&
+        (oldDelegate.color != color ||
             oldDelegate.thickness != thickness ||
             oldDelegate.orientation != orientation);
   }
