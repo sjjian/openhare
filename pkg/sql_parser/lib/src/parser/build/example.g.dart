ac4a837518427ea680272bb76e456a20cae66059
Sun Jian <921465802@qq.com>
2025-11-22 16:39:22 +0800

    从git移除gen code

--- pkg/sql_parser/lib/src/parser/build/example.g.dart
+++
@@ -1,9 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-part of 'example.dart';
-
-// **************************************************************************
-// PropertySumGenerator
-// **************************************************************************
-
-num allSum() => answer + tau + sun + sun2;
