0b773dc0994838f49547d8f4aa534c8e54289c5a
Sun Jian <921465802@qq.com>
2025-11-22 17:06:55 +0800

    windows 关闭按钮样式优化

--- client/lib/screens/page_skeleton.dart
+++ client/lib/screens/page_skeleton.dart
@@ -1,55 +1,41 @@
 import 'package:client/screens/app.dart';
 import 'package:client/widgets/const.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:sql_editor/re_editor.dart';
 import 'package:window_manager/window_manager.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:client/widgets/divider.dart';
 
-// todo: 在windows上需要添加窗口按钮
-final buttonColors = WindowButtonColors(
-    iconNormal: const Color(0xFF805306),
-    mouseOver: const Color(0xFFF6A00C),
-    mouseDown: const Color(0xFF805306),
-    iconMouseOver: const Color(0xFF805306),
-    iconMouseDown: const Color(0xFFFFD500));
-
-final closeButtonColors = WindowButtonColors(
-    mouseOver: const Color(0xFFD32F2F),
-    mouseDown: const Color(0xFFB71C1C),
-    iconNormal: const Color(0xFF805306),
-    iconMouseOver: Colors.white);
-
 class WindowButtons extends StatelessWidget {
   const WindowButtons({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context).colorScheme;
+    final colors = WindowButtonColors(
+      iconNormal: theme.onSurface, // 深灰色图标
+      mouseOver: theme.surfaceContainerHigh, // 浅灰色背景（悬停）
+      mouseDown: theme.surfaceContainerHigh, // 稍深的灰色背景（按下）
+      iconMouseOver: theme.onSurface, // 悬停时图标保持深灰色
+      iconMouseDown: theme.onSurface, // 按下时图标保持深灰色
+    );
+    final closeColors = WindowButtonColors(
+      iconNormal: theme.onSurface, // 深灰色图标
+      mouseOver: theme.error, // 浅灰色背景（悬停）
+      mouseDown: theme.error, // 稍深的灰色背景（按下）
+      iconMouseOver: theme.onError, // 悬停时图标保持深灰色
+      iconMouseDown: theme.onError, // 按下时图标保持深灰色
+    );
     return Row(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        MinimizeWindowButton(colors: buttonColors),
-        MaximizeWindowButton(colors: buttonColors),
-        CloseWindowButton(colors: closeButtonColors),
+        MinimizeWindowButton(colors: colors),
+        MaximizeWindowButton(colors: colors),
+        CloseWindowButton(colors: closeColors),
       ],
     );
   }
 }
-
-// class DragToMoveArea2 extends StatelessWidget {
-//   final Widget child;
-//   const DragToMoveArea2({super.key, required this.child});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return GestureDetector(
-//       behavior: HitTestBehavior.translucent,
-//       onPanStart: (details) {
-//         windowManager.startDragging();
-//       },
-//       child: child,
-//     );
-//   }
-// }
 
 class MoveWindows extends StatelessWidget {
   const MoveWindows({
